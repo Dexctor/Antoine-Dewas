38aca5f17df4099203b2d00a7bb347370f0be7cb
dexctor <contactantoinedewas@gmail.com>
2025-01-17 23:51:08 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from '@/components/ui/sonner';
-
+import { Analytics } from '@vercel/analytics/react';
 // Lazy load components
 const Index = lazy(() => import("./pages/Index"));
 const NotFound = lazy(() => import("./pages/NotFound"));
@@ -30,6 +30,7 @@ const App = () => (
     <TooltipProvider>
       <BrowserRouter>
         <Suspense fallback={<PageLoader />}>
+        <Analytics />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
